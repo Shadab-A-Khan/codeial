6b54637f62f375abfb4fff0a9377c5c1b196afd4
Shadab Ahmad Khan <shadabahmadkhan4@gmail.com>
2022-07-11 11:08:45 +0530

    starting with multer

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/upload/users/avatars')
 const userSchema =new mongoose.Schema({
     email: {
         type: String,
